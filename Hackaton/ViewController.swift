c8d0d4ba30c38d8c5409505622e3e1be105eeb43
Robson Fernandes <robson.fernandes@gmail.com>
2018-03-16 10:28:55 -0300

    Initial Commit

---
+++ Hackaton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hackaton
+//
+//  Created by Robson Fernandes on 16/03/18.
+//  Copyright © 2018 Robson Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HackatonTests/HackatonTests.swift
@@ -0,0 +1,36 @@
+//
+//  HackatonTests.swift
+//  HackatonTests
+//
+//  Created by Robson Fernandes on 16/03/18.
+//  Copyright © 2018 Robson Fernandes. All rights reserved.
+//
+
+import XCTest
+@testable import Hackaton
+
+class HackatonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
